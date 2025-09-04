a51bc720a771716d48925bb43ba3fa3927234988
Raju Barman <rajubarman7665@gmail.com>
2025-09-04 18:44:34 +0530

    first commit

---
+++ src/db/db.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+
+function connectDB(){
+    mongoose.connect(process.env.MONGODB_URL)
+    .then(()=>{
+        console.log("Connected to MongoDB")
+    })
+    .catch((error)=>{
+        console.log("Error connecting to MongoDB:", error)
+    })
+}
+module.exports = connectDB;
---
+++ src/routes/auth.routes.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const userModel = require('../models/user.model')
+const router = express.Router()
+
+
+router.post('/register',async (req,res)=>{
+
+    const {username,password}= req.body
+    const user = await userModel.create({
+        username,password
+    })
+    res.status(201).json({
+        message:"User registered successfully",
+        user
+    })
+})
+router.post('/login', async (req,res)=>{
+    const {username,password}= req.body
+    //now check if user exists
+    const user = await userModel.findOne({
+        username:username
+    })
+    if(!user){
+        return res.status(401).json({
+            message : "user account not found [Invalid Username]"
+
+        })
+    }
+
+    const isPasswordValid = password === user.password
+    if(!isPasswordValid){
+        return res.status(401).json({
+            message : "Invalid Password"
+        })
+    }
+    res.status(200).json({
+        message : "Login Successful"
+    })
+} )
+
+module.exports= router;
